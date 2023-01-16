ef97365d4322f36e215d720589388c67daedfc34
Sophia De La Rosa <sophial.delarosa@gmail.com>
2023-01-16 07:31:47 -0500

    Properly sizing the columns with a responsive photo, this time from scratch.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,10 @@ function Home() {
         <div>
             <div class="title">Bio</div>
             <div class="row">
-                <div class="column">
+                <div class="columnleft">
                     <img id="profilepic" /> 
                 </div>
-                <div class="column">
+                <div class="columnright">
                     <p> Hai! I'm Sophia. I have a Bachelor of Science in Journalism and International Relations and am currently working toward my web development certification.</p>
                     <p> I currently work as a video editor at CNN and stream video games on Twitch with the computer I built.</p>
                     <p> I'm looking forward to learning more about coding and getting into it full-time!</p>
