2a4adb228501421aaf24a704fb2399f071708ff5
Sophia De La Rosa <sophial.delarosa@gmail.com>
2023-01-16 07:26:29 -0500

    Restoring original column percentages.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,10 @@ function Home() {
         <div>
             <div class="title">Bio</div>
             <div class="row">
-                <div class="columnleft">
+                <div class="column">
                     <img id="profilepic" /> 
                 </div>
-                <div class="columnright">
+                <div class="column">
                     <p> Hai! I'm Sophia. I have a Bachelor of Science in Journalism and International Relations and am currently working toward my web development certification.</p>
                     <p> I currently work as a video editor at CNN and stream video games on Twitch with the computer I built.</p>
                     <p> I'm looking forward to learning more about coding and getting into it full-time!</p>
