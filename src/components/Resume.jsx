586f727d2fe90286774dcfef24df168f5a6861c9
Sophia De La Rosa <sophial.delarosa@gmail.com>
2023-01-16 10:27:08 -0500

    Resume styling changes.

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -17,10 +17,11 @@ function Resume() {
                         <h3>CNN Atlanta</h3>
                     </div>
                     <div class="rightcolumn">
-                        <p>-- 02/2019 to present --</p>
+                        <p>-- 04/2019 to present --</p>
                     </div>  
                 </div>
                 <p>Production assistant and news editor </p>
+                <p>Edited and distributed news video to CNN affiliates using Premiere Pro. </p>
 
                 <h3>KVUE News ABC, Austin</h3>
                 <p class="description">Lead news editor, daybreak and nightbeat newscasts -- 02/2018 to 04/2019</p>
