9c97ddfb8ffd658d1802211a17f5f4338d3047c2
Sophia De La Rosa <sophial.delarosa@gmail.com>
2023-01-16 09:54:35 -0500

    Updated resume download link. Testing new resume page formatting.

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/Resume.css';
-import resume from "../assets/SophiaDeLaRosaResume.pdf";
+import resume from "../assets/SophiaDeLaRosa.pdf";
 
 function Resume() {
     return (
@@ -8,16 +8,26 @@ function Resume() {
         <div>
             <div>
                 <div class="title">Resume</div>
-                <div><a href={resume}>Please download my CV here</a></div>
+                <div><a href={resume}>Download my resume here.</a></div>
             </div>
             <div>
-                <h1>WORK EXPERIENCE</h1>
-                <h3>CNN Atlanta</h3>
-                <p class="description">Production assistant and news editor -- 02/2019 to present</p>
+                <h1>EXPERIENCE</h1>
+                <div class="row">
+                    <div class="leftcolumn">
+                        <h3>CNN Atlanta</h3>
+                    </div>
+                    <div class="rightcolumn">
+                        <p>-- 02/2019 to present --</p>
+                    </div>
+                </div>
+                <p class="description">Production assistant and news editor </p>
+
                 <h3>KVUE News ABC, Austin</h3>
                 <p class="description">Lead news editor, daybreak and nightbeat newscasts -- 02/2018 to 04/2019</p>
+
                 <h3>CNN Atlanta</h3>
                 <p class="description">Intern, Southeast Regional Bureau 06/2017 to 07/2017</p>
+
                 <h3>University Star, university newspaper</h3>
                 <p class="description">Photographer</p>
 
@@ -33,12 +43,7 @@ function Resume() {
                 <p class="description">Yearbook staff Lead Photo Editor and Head Photojournalist</p>
                 <p class="description">AP classes: English, Studio Art</p>
 
-                <h1>Awards & Achievements</h1>
-                <p class="description">1st place in Mixed Media -- Advanced Studio Art piece</p>
-                <p class="description">1st Place Women's 13.5 meter competition -- Archery</p>
-                <p class="description">Distinguished Student Award -- Fine Arts</p>
-
-                <h1>Interests & Skills</h1>
+                <h1>TECHNICAL SKILLS</h1>
                 <p class="description">Georgia Tech certified full stack web developer</p>
                 <p class="description">Technologies: HTML, CSS, JavaScript, Bootstrap</p>
                 <p class="description">Adobe Suite: Premiere Pro, Photoshop, Illustrator</p>
@@ -48,6 +53,12 @@ function Resume() {
                 <p class="description">Photography</p>
                 <p class="description">PADI certified scuba diver</p>
                 <p class="description">Archery</p>
+                <p class="description">1st place in Mixed Media -- Advanced Studio Art piece</p>
+                <p class="description">1st Place Women's 13.5 meter competition -- Archery</p>
+                <p class="description">Distinguished Student Award -- Fine Arts</p>
+
+                <h1>REFERENCES</h1>
+                
             </div>
         </div>
 
