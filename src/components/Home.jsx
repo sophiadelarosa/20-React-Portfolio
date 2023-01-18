1a8fd319c530f31a4f634594b988356b4b75e9e5
Sophia De La Rosa <sophial.delarosa@gmail.com>
2023-01-18 08:40:04 -0500

    Changes to bio on the home page. Making header tweaks.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
                     <img id="profilepic" height="250px" width="250px"/> 
                 </div>
                 <div class="columnright">
-                    <p> Hai! I'm Sophia. I have a Bachelor of Science in Journalism and International Relations and am currently working toward my web development certification.</p>
+                    <p> Hai! I'm Sophia. I have a Bachelor of Science in Journalism and International Relations and recently got my Full Stack web development certification from Georgia Institute of Technology.</p>
                     <p> I currently work as a video editor at CNN and stream video games on Twitch with the computer I built.</p>
                     <p> I'm looking forward to learning more about coding and getting into it full-time!</p>
                 </div>
